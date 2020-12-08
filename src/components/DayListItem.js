1b0e8482e22a65326b22895969ae3c86e4d51a3c
Connie <leungcnie@gmail.com>
2020-12-07 16:55:41 -0800

    Add logic for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+import classnames from 'classnames';
+
 export default function DayListItem(props) {
+  const listClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={listClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
+
