12a4e3838bf09bfef51b8f2ff05192d1da97768f
Connie <leungcnie@gmail.com>
2020-12-07 17:13:41 -0800

    Add function formatSpots to return corresponding string for DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,24 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
+
+  const formatSpots = (spots) => {
+    switch (spots) {
+      case 0:
+        return "no spots remaining";
+        break;
+      case 1:
+        return "1 spot remaining";
+        break;
+      default:
+        return `${spots} spots remaining`;
+    }
+  }
+
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
