ca635962fa4f10f1e4109d168d4d8cc43a2387d2
Connie <leungcnie@gmail.com>
2020-12-07 22:59:46 -0800

    Add logic and CSS for InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewersList = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={props.setInterviewer}
+        selected = {interviewer.id === props.interviewer}
+      />
+    )
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewersList}
+      </ul>
+    </section>
+  );
+};
