cc3c006461ff5ac4cef102ed7fb8907d74ba3f02
Connie <leungcnie@gmail.com>
2020-12-10 19:26:58 -0800

    Add getInterviewersForDay selector and add tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,7 @@
 export function getAppointmentsForDay(state, day) {
   const result = [];
   const dayObj = state.days.filter(obj => obj.name === day)[0]; // Get specific day object
-  const appointments = dayObj? [...dayObj.appointments] : []; // Get appointments array if that day exists
+  const appointments = dayObj ? [...dayObj.appointments] : []; // Get appointments array if that day exists
 
   // If there are no appointments or the day doesn't exist, return an empty array
   if (appointments.length === 0) {
@@ -11,6 +11,25 @@ export function getAppointmentsForDay(state, day) {
   for (const id of appointments) {
     if (state.appointments[id]) {
       result.push(state.appointments[id]);
+    }
+  }
+
+  return result;
+}
+
+export function getInterviewersForDay(state, day) {
+  const result = [];
+  const dayObj = state.days.filter(obj => obj.name === day)[0]; // Get specific day object
+  const interviewers = dayObj ? [...dayObj.interviewers] : []; // Get interviewers array if that day exists
+
+  // If there are no interviewers or the day doesn't exist, return an empty array
+  if (interviewers.length === 0) {
+    return [];
+  }
+
+  for (const id of interviewers) {
+    if (state.interviewers[id]) {
+      result.push(state.interviewers[id]);
     }
   }
 
