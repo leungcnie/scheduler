d8ccb59a55835a6ad4ff72bfa66010a70d42cd8b
Connie <leungcnie@gmail.com>
2020-12-14 12:03:26 -0800

    Install PropTypes and update InterviewerList to require the interviewers prop to be an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
@@ -25,3 +26,9 @@ export default function InterviewerList(props) {
     </section>
   );
 };
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
