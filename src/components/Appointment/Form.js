ebc9caac416c005853b564698162b15d25cfd275
Connie <leungcnie@gmail.com>
2020-12-14 14:13:56 -0800

    Add 4 tests for Form component and replace save function with validate function to prevent blank input

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
@@ -6,18 +6,24 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const cancel = () => {
     reset();
     props.onCancel();
-  }
-  const save = () => {
-    props.onSave(name, interviewer);
-  }
+  };
   const reset = () => {
     setName("");
     setInterviewer(null);
-  }
+  };
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -30,22 +36,29 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
-          /*
+            data-testid="student-name-input"
+            /*
             This must be a controlled component
           */
           />
         </form>
-        <InterviewerList 
-          interviewers={props.interviewers} 
-          value={interviewer}  
-          onChange={setInterviewer} />
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
-  )
+  );
 }
