a6352fd40ea3640d89db6d4077fb5988254c480a
Connie <leungcnie@gmail.com>
2020-12-14 21:05:21 -0800

    Remove dead code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -19,32 +19,3 @@ export default function InterviewListItem(props) {
     </li>
   );
 };
-
-// import React, { useState } from 'react';
-
-// import "components/InterviewerListItem.scss"
-// import classnames from 'classnames';
-
-// export default function InterviewListItem(props) {
-//   const [selected, setSelected] = useState(props.selected);
-
-//   const listClass = classnames("interviewers__item", {
-//     "interviewers__item--selected": selected
-//   });
-
-//   const selectInterviewer = () => {
-//     props.setInterviewer(props.id);
-//     setSelected(true);
-//   }
-
-//   return (
-//     <li className={listClass} onClick={selectInterviewer}>
-//       <img
-//         className="interviewers__item-image"
-//         src={props.avatar}
-//         alt={props.name}
-//       />
-//       {selected && props.name}
-//     </li>
-//   );
-// };
