5df310f55eaa7949ab5c90d82b29ceced79e2764
Connie <leungcnie@gmail.com>
2020-12-14 20:08:28 -0800

    Update Application test to use async await syntax instead of Promise chaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,8 +9,9 @@ afterEach(cleanup);
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
